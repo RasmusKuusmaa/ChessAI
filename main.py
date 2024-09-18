1ed5c35f50102a88facc707fb344984c243b4f3d
77f315 <77f315@gmail.com>
2024-09-18 17:49:55 +0300

    techincally functional, but very bad chess bot

--- main.py
+++ main.py
@@ -17,6 +17,44 @@ class Main:
             eval += len(self.board.pieces(type, ch.WHITE)) * vals[type]
             eval -= len(self.board.pieces(type, ch.BLACK)) * vals[type]
         return eval
+
+    def minimax(self, depth, is_maximizing):
+        if depth == 0 or self.board.is_game_over():
+            return self.evaluate()
+        if is_maximizing:
+            max_eval = -float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                eval = self.minimax(depth - 1, False)
+                self.board.pop()
+                max_eval = max(max_eval, eval)
+            return max_eval
+        else:
+            min_eval = float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                eval = self.minimax(depth - 1, True)
+                self.board.pop()
+                min_eval = min(min_eval, eval)
+            return min_eval
+
+    def best_move(self, depth):
+        best_move = None
+        best_val = -float('inf') if self.board.turn == ch.WHITE else float('inf')
+        for move in self.board.legal_moves:
+            self.board.push(move)
+            board_val = self.minimax(depth - 1, not self.board.turn)
+            self.board.pop()
+            if self.board.turn:
+                if board_val > best_val:
+                    best_val = board_val
+                    best_move = move
+            else:
+                if board_val < best_val:
+                    best_val = board_val
+                    best_move = move
+        return best_move
+
     def hooman(self):
         print(self.board)
         try:
@@ -43,10 +81,11 @@ class Main:
                 if self.board.turn:
                     self.hooman()
                 else:
-                    self.hooman2()
+                    self.board.push(self.best_move(depth = 2))
             res = self.board.result()
             print(res)
         except KeyboardInterrupt:
             print("exit")
+
 game = Main()
 game.start_game()
