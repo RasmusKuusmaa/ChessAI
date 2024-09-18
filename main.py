939ca870bcce5f25d06bdb8a009fe008a4ef63a9
77f315 <77f315@gmail.com>
2024-09-18 17:02:26 +0300

    added a terrrible eval function that just counts up material

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ class Main:
     def __init__(self):
         self.board = ch.Board()
 
+    def evaluate(self):
+        vals = {
+            ch.ROOK: 5,
+            ch.PAWN: 1,
+            ch.BISHOP: 3,
+            ch.KNIGHT: 3,
+            ch.QUEEN: 9,
+        }
+        eval = 0
+        for type in vals:
+            eval += len(self.board.pieces(type, ch.WHITE)) * vals[type]
+            eval -= len(self.board.pieces(type, ch.BLACK)) * vals[type]
+        return eval
     def hooman(self):
         print(self.board)
         try:
@@ -25,6 +38,7 @@ class Main:
     def start_game(self):
         try:
             while not self.board.is_game_over():
+                print(self.evaluate())
                 print(self.board.legal_moves)
                 if self.board.turn:
                     self.hooman()
