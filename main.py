94e51a3e5ab30d86984cf9aeb9fd4960cc9364fc
77f315 <77f315@gmail.com>
2024-09-18 16:56:30 +0300

    added a way to close the game"

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import chess as ch
 class Main:
     def __init__(self):
         self.board = ch.Board()
-    
+
     def hooman(self):
         print(self.board)
         try:
             play = input("whites turn: ")
             self.board.push_san(play)
-        except:
+        except ValueError:
             print('nope, thats not allowed')
             self.hooman()
 
@@ -18,20 +18,21 @@ class Main:
         try:
             play = input("blacks turn: ")
             self.board.push_san(play)
-        except:
+        except ValueError:
             print('nope, thats not allowed')
             self.hooman2()
 
     def start_game(self):
-        while not self.board.is_game_over():
-            print(self.board.legal_moves)
-            # self.board.turn returns true if it is whites turn
-            if self.board.turn:
-                self.hooman()
-            else:
-                self.hooman2()
-
-        res = self.board.result()
-        print(res)
+        try:
+            while not self.board.is_game_over():
+                print(self.board.legal_moves)
+                if self.board.turn:
+                    self.hooman()
+                else:
+                    self.hooman2()
+            res = self.board.result()
+            print(res)
+        except KeyboardInterrupt:
+            print("exit")
 game = Main()
 game.start_game()
